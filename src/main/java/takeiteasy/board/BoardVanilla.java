d0ac4594a3d38da1fa31b48827da464fee0068d1
azad <azad.sadr.h@gmail.com>
2021-01-29 19:00:16 +0100

    outofboard implemented

--- src/main/java/takeiteasy/board/BoardVanilla.java
+++ src/main/java/takeiteasy/board/BoardVanilla.java
@@ -3,6 +3,13 @@ package takeiteasy.board;
 import takeiteasy.tilepool.Tile;
 
 public class BoardVanilla implements IBoard {
+
+    private Boolean areCoordinatesInRange(HexCoordinates coordinates){
+        return (-3<coordinates.getX() && coordinates.getX()<3) &&
+               (-3<coordinates.getY() && coordinates.getY()<3) &&
+               (-3<coordinates.getZ() && coordinates.getZ()<3);
+    }
+
     @Override
     public void placeTile(Tile tile, HexCoordinates coordinates) {
 
@@ -10,9 +17,12 @@ public class BoardVanilla implements IBoard {
 
     @Override
     public Tile getTile(HexCoordinates coordinates) throws OutOfBoardCoordinatesException {
-        throw new OutOfBoardCoordinatesException();
-        //return null;
+        if (!this.areCoordinatesInRange(coordinates)) {
+            throw new OutOfBoardCoordinatesException();
+        }
+        return null;
     }
+
 
     @Override
     public Integer computeScore() {
