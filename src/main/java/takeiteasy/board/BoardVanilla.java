7c7c47a8fb5f32598e8d496090d0afc8c7fe0028
azad <azad.sadr.h@gmail.com>
2021-01-29 17:58:23 +0100

    BoardVanilla empty class created

---
+++ src/main/java/takeiteasy/board/BoardVanilla.java
@@ -0,0 +1,20 @@
+package takeiteasy.board;
+
+import takeiteasy.tilepool.Tile;
+
+public class BoardVanilla implements IBoard {
+    @Override
+    public void placeTile(Tile tile, HexCoordinates coordinates) {
+
+    }
+
+    @Override
+    public Tile getTile(HexCoordinates coordinates) {
+        return null;
+    }
+
+    @Override
+    public Integer computeScore() {
+        return null;
+    }
+}
