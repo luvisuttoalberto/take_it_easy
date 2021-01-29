3ba2627ddeb6a5400d53a6c57029d9b9ceca7dc4
azad <azad.sadr.h@gmail.com>
2021-01-29 17:52:46 +0100

    IBoard updated

--- src/main/java/takeiteasy/board/IBoard.java
+++ src/main/java/takeiteasy/board/IBoard.java
@@ -1,4 +1,8 @@
 package takeiteasy.board;
+import takeiteasy.tilepool.Tile;
 
 public interface IBoard {
+    void placeTile(Tile tile, HexCoordinates coordinates);
+    Tile getTile(HexCoordinates coordinates);
+    Integer computeScore();
 }
