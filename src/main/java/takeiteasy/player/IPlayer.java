aea27bdfdf491b2fefca817ddfc30a44f433ada0
azad <azad.sadr.h@gmail.com>
2021-01-31 19:03:10 +0100

    IPlayer interface created

--- src/main/java/takeiteasy/player/IPlayer.java
+++ src/main/java/takeiteasy/player/IPlayer.java
@@ -1,4 +1,19 @@
 package takeiteasy.player;
+import takeiteasy.board.*;
+import takeiteasy.tilepool.Tile;
 
 public interface IPlayer {
+    State getState();
+    void setName(String name);
+    String getName();
+    void placeTile(Tile tile) throws BadHexCoordinatesException, OutOfBoardCoordinatesException, CoordinatesOccupidedException;
+    void leaveTheMatch();
+    Tile showTileFromBoardAtCoordinates(HexCoordinates coordinates);
+    Integer computeScore();
+    public enum State {
+        Placing,
+        WaitOther,
+        WaitMatch,
+        Leave;
+    }
 }
