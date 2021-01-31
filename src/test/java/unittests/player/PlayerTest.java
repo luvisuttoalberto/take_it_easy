b77431b0503dc53905c61e5ab7d23b65c5bd7f50
azad <azad.sadr.h@gmail.com>
2021-01-31 19:05:27 +0100

    Plyer test created

--- src/test/java/unittests/player/PlayerTest.java
+++ src/test/java/unittests/player/PlayerTest.java
@@ -1,4 +1,25 @@
 package unittests.player;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import takeiteasy.board.BoardVanilla;
+import takeiteasy.player.Player;
+import takeiteasy.tilepool.Tile;
+
 public class PlayerTest {
+    @Test
+    public void testGetSetName() {
+        Player p = new Player("Casagrande");
+        p.setName("Medvet");
+        Assertions.assertEquals("Medvet", p.getName());
+    }
+
+    @Test
+    public void testPlaceTile() {
+        Player p = new Player("Tornatore");
+        Tile tile = new Tile(2, 3, 4);
+        BoardVanilla b = new BoardVanilla();
+        Assertions.assertEquals(1,1);
+
+    }
 }
