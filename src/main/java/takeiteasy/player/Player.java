0047d238784991e2ef97f520bf7e49cdaeac87ff
azad <azad.sadr.h@gmail.com>
2021-01-31 19:04:24 +0100

    Player class created and implemented- still working

---
+++ src/main/java/takeiteasy/player/Player.java
@@ -0,0 +1,63 @@
+package takeiteasy.player;
+
+import takeiteasy.board.*;
+import takeiteasy.tilepool.Tile;
+
+import java.util.Scanner;
+
+public class Player implements IPlayer{
+
+    private String Name;
+    private State playerState;
+    BoardVanilla playerBoard;
+
+    public Player(String name) {
+        Name = name;
+        this.playerState = State.WaitMatch;
+        this.playerBoard = new BoardVanilla();
+    }
+
+    @Override
+    public State getState() {
+        return playerState;
+    }
+
+    @Override
+    public void setName(String name) {
+        Name = name;
+    }
+
+    @Override
+    public String getName() {
+        return Name;
+    }
+
+    @Override
+    public void placeTile(Tile tile) throws BadHexCoordinatesException, OutOfBoardCoordinatesException, CoordinatesOccupidedException {
+        HexCoordinates coordinates = getCoordinatesFromUser();
+        playerBoard.placeTile(tile, coordinates);
+    }
+
+    @Override
+    public void leaveTheMatch() {
+        playerState = State.Leave;
+    }
+
+    @Override
+    public Tile showTileFromBoardAtCoordinates(HexCoordinates coordinates) {
+        return playerBoard.getTile(coordinates);
+    }
+
+    @Override
+    public Integer computeScore() {
+        return playerBoard.computeScore();
+    }
+
+    private HexCoordinates getCoordinatesFromUser() throws BadHexCoordinatesException {
+        Scanner sc = new Scanner(System.in);
+        Integer index1 = sc.nextInt();
+        Integer index2 = sc.nextInt();
+        Integer index3 = sc.nextInt();
+        return new HexCoordinates(index1, index2, index3);
+    }
+}
