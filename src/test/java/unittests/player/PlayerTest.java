1a25ae6205242619dfb5264114a13fca2bb0f9c6
azad <azad.sadr.h@gmail.com>
2021-02-01 17:50:45 +0100

    added testPlaceTile() of player class

--- src/test/java/unittests/player/PlayerTest.java
+++ src/test/java/unittests/player/PlayerTest.java
@@ -2,7 +2,7 @@ package unittests.player;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import takeiteasy.board.BoardVanilla;
+import takeiteasy.board.*;
 import takeiteasy.player.IPlayer;
 import takeiteasy.player.OutOfProperStateException;
 import takeiteasy.player.Player;
@@ -18,11 +18,14 @@ public class PlayerTest {
     }
 
     @Test
-    public void testPlaceTile() {
+    public void testPlaceTile() throws OutOfBoardCoordinatesException, CoordinatesOccupidedException, BadHexCoordinatesException, OutOfProperStateException {
         Player p = new Player("Tornatore");
-        Tile tile = new Tile(2, 3, 4);
-        BoardVanilla b = new BoardVanilla();
-        Assertions.assertEquals(1,1);
+        p.startMatch();
+        Tile expectedTile = new Tile(1, 2, 3);
+        HexCoordinates c = new HexCoordinates(0,0,0);
+        p.placeTile(expectedTile, c);
+        Tile realTile = p.showTileFromBoardAtCoordinates(c);
+        Assertions.assertEquals(expectedTile,realTile);
 
     }
 }
