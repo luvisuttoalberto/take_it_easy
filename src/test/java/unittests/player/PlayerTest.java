a500dcf49b612dde3d4721bc4d8d12e498c8e7f6
azad <azad.sadr.h@gmail.com>
2021-02-02 15:09:20 +0100

    added and passed testEndMatch() for player class

--- src/test/java/unittests/player/PlayerTest.java
+++ src/test/java/unittests/player/PlayerTest.java
@@ -10,7 +10,6 @@ import takeiteasy.tilepool.Tile;
 import takeiteasy.tilepool.TilePool;
 
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Random;
 
@@ -72,9 +71,56 @@ public class PlayerTest {
     }
 
     @Test
-    public void testEndMatch() throws OutOfProperStateException {
-        Player p = new Player("Sartori");
+    public void testEndMatch() throws BadHexCoordinatesException, OutOfProperStateException, OutOfBoardCoordinatesException, CoordinatesOccupidedException {
+        Player p = new Player("Kavka");
+        TilePool tp = new TilePool(1);
+
+        int[][] arr = {
+                {-1, 2, -1},
+                {1, 1, -2},
+                {0,0,0},
+
+                {-2, 2, 0},
+                {-1, 1, 0},
+                {-1, 0, 1},
+                {-1, -1, 2},
+
+                {0, 2, -2},
+                {0, 1, -1},
+                {-2, 0, 2},
+                {0, -1, 1},
+                {0,-2, 2},
+
+
+                {-2, 1, 1},
+                {1, 0, -1},
+                {1, -1, 0},
+                {1, -2, 1},
+
+                {2, 0, -2},
+                {2, -1, -1},
+                {2, -2, 0}
+        };
+        Random rand = new Random(10);
+        Collections.shuffle(Arrays.asList(arr), rand);
+        HexCoordinates[] coords = new HexCoordinates[19];
+        for (int i=0; i<19; ++i) {
+            int x = arr[i][0];
+            int y = arr[i][1];
+            int z = arr[i][2];
+            coords[i] = new HexCoordinates(x, y, z);
+        }
+
         p.startMatch();
+        for (int i=0; i<19; ++i) {
+            Tile tile = tp.getTile(i);
+            p.placeTile(tile, coords[i]);
+            if(i==18) break;
+            p.transitionFromWaitingPlayersToPlacing();
+        }
+        p.computeScore();
+        p.endMatch();
+        Assertions.assertEquals(IPlayer.State.WaitMatch,p.getState());
     }
 
     @Test
