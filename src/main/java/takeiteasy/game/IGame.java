f90a0f3e23aa84ca03db1300b145a0adeb8dcb4c
nickplas <plasencia.milton@gmail.com>
2021-02-06 18:30:16 +0100

    Game inteface and JSON library added

--- src/main/java/takeiteasy/game/IGame.java
+++ src/main/java/takeiteasy/game/IGame.java
@@ -1,4 +1,24 @@
 package takeiteasy.game;
 
+import takeiteasy.board.HexCoordinates;
+import org.json.JSONObject;
+
 public interface IGame {
+    //main menu
+    void createLocalGame();
+    void exitGame();
+
+    //local game setup
+    void addPlayer(String name);
+    void removePlayer(String name);
+    void renamePlayer(String oldName, String newName);
+    void startLocalMatch();
+    void setMatchSeed(long seed);
+    void backToTheMainMenu();
+
+    //Play local match
+    void playerPlacesTileAt(String name, HexCoordinates coordinates);
+    void backToLocalSetup();
+
+    JSONObject getData();
 }
