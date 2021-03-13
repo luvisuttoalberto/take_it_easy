9bdca34589cb1f7d490d0a0c13f041a1e1fcd62f
luvisuttoalberto <luvisutto.alberto@gmail.com>
2021-03-13 17:37:03 +0100

    Create local game test passed

--- src/test/java/unittests/game/GameTest.java
+++ src/test/java/unittests/game/GameTest.java
@@ -14,4 +14,13 @@ public class GameTest {
         assertNull(game.getData());
     }
 
+    @Test
+    public void testCreateLocalLobby(){
+        Game game = new Game();
+        game.createLocalGame();
+        JSONObject data = game.getData();
+        assertNull(data.opt("message"));
+        assertEquals(data.get("gameState"), "LOCAL_LOBBY");
+    }
+
 }
