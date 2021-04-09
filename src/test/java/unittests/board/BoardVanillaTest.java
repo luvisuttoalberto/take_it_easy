2a4d9f208cfef80ebcab84138010cb1674afdfb3
nickplas <plasencia.milton@gmail.com>
2021-04-09 15:07:44 +0200

    getData() tested properly in Player

--- src/test/java/unittests/board/BoardVanillaTest.java
+++ src/test/java/unittests/board/BoardVanillaTest.java
@@ -125,6 +125,5 @@ public class BoardVanillaTest {
         }
         catch(Exception ignored){
         }
-
     }
 }
--- src/test/java/unittests/player/PlayerTest.java
+++ src/test/java/unittests/player/PlayerTest.java
@@ -16,11 +16,36 @@ import static unittests.utility.Utility.*;
 public class PlayerTest {
 
     @Test
+    public void testGetData(){
+        Player player = new Player("Carlos");
+        Integer score = 54;
+        try {
+            ArrayList<Pair<Tile, HexCoordinates>> list = getTilesAndCoordinatesBoard11(score);
+            player.startMatch();
+            JSONObject playerData = player.getData();
+            assertEquals("PLACING", playerData.get("playerState"));
+            for(int i = 0; i < list.size(); ++i) {
+                player.placeTile(list.get(i).tile, list.get(i).coordinate);
+                if (i == list.size() - 1) break;
+                playerData = player.getData();
+                assertEquals("WAIT_OTHER", playerData.get("playerState"));
+                player.transitionFromWaitingPlayersToPlacing();
+            }
+            player.endMatch();
+            playerData = player.getData();
+            assertEquals("WAIT_MATCH", playerData.get("playerState"));
+            assertEquals(score, playerData.get("playerScore"));
+        }
+        catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
     public void testStartMatch(){
         Player player = new Player("Dario");
         try {
             player.startMatch();
-            assertEquals("PLACING", player.getData().get("playerState"));
             assertEquals(IPlayer.State.PLACING, player.getState());
         }
         catch (Exception e) {
@@ -45,7 +70,6 @@ public class PlayerTest {
                                             insertedTileData.getInt("right")
                                         );
 //            Tile realTile = player.showTileFromBoardAtCoordinates(coords);
-            assertEquals("WAIT_OTHER", player.getData().get("playerState"));
             assertEquals(IPlayer.State.WAIT_OTHER, player.getState());
             assertEquals(expectedTile, insertedTile);
         }
@@ -63,7 +87,6 @@ public class PlayerTest {
             player.startMatch();
             player.placeTile(tile, coordinates);
             player.transitionFromWaitingPlayersToPlacing();
-            assertEquals("PLACING", player.getData().get("playerState"));
             assertEquals(IPlayer.State.PLACING, player.getState());
         }
         catch (Exception e) {
@@ -79,13 +102,11 @@ public class PlayerTest {
             player.startMatch();
             for(int i = 0; i < list.size(); ++i) {
                 player.placeTile(list.get(i).tile, list.get(i).coordinate);
-                if (i == 18) break; //TODO: use list.size()
+                if (i == list.size() - 1) break;
                 player.transitionFromWaitingPlayersToPlacing();
             }
 //            player.computeScore();
             player.endMatch();
-            //TODO: should we test it twice?
-//            assertEquals("WAIT_MATCH", player.getData().get("playerState"));
             assertEquals(IPlayer.State.WAIT_MATCH, player.getState());
         }
         catch (Exception e) {
@@ -104,8 +125,6 @@ public class PlayerTest {
             player.reset();
 //            Tile realTile = player.showTileFromBoardAtCoordinates(coordinates);
             JSONObject data = player.getData();
-            //TODO: should we test it twice?
-//            assertEquals("WAIT_MATCH", data.get("playerState"));
             assertEquals(IPlayer.State.WAIT_MATCH, player.getState());
             assertTrue(data.getJSONObject("playerBoard").isEmpty());
 //            Assertions.assertNull(realTile);
@@ -114,8 +133,6 @@ public class PlayerTest {
             fail();
         }
     }
-
-    //TODO:should we remove this? player doesn't have a computeScore method anymore
 //    @Test
 //    public void testComputeScore() {
 //        Integer score = 54;
