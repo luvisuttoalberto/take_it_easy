3dc927977cdc85dfc168fccde804acb5e7b600bb
nickplas <plasencia.milton@gmail.com>
2021-04-13 17:27:09 +0200

    badCoordinateTest refactored using assertThrows in HexCoordinatesTest

--- src/test/java/unittests/board/HexCoordinatesTest.java
+++ src/test/java/unittests/board/HexCoordinatesTest.java
@@ -2,18 +2,12 @@ package unittests.board;
 import takeiteasy.board.*;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class HexCoordinatesTest {
 
     @Test
     public void badCoordinatesTest(){
-        try {
-            HexCoordinates hc = new HexCoordinates(1, 2, 3);
-            fail();
-        }
-        catch (BadHexCoordinatesException ignored){
-            //test passed
-        }
+        assertThrows(BadHexCoordinatesException.class, ()-> new HexCoordinates(1,2,3));
     }
 }
