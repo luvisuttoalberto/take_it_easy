cd021d5aa69e09ea37d3c7ba51684df985116288
azad <azad.sadr.h@gmail.com>
2021-05-22 20:00:24 +0200

    added credits page and its controller

---
+++ src/main/java/takeiteasy/GUI/Controller/CreditsCtrl.java
@@ -0,0 +1,8 @@
+package takeiteasy.GUI.Controller;
+
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+public class CreditsCtrl {
+}
--- src/main/java/takeiteasy/GUI/Controller/MainMenuCtrl.java
+++ src/main/java/takeiteasy/GUI/Controller/MainMenuCtrl.java
@@ -3,10 +3,16 @@ package takeiteasy.GUI.Controller;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 import org.json.JSONObject;
 import takeiteasy.GUI.IViewUpdater;
 import takeiteasy.game.IGame;
+
+import java.io.IOException;
 
 //TODO: how about implementing the initializable class
 public class MainMenuCtrl implements IViewController{
@@ -40,12 +46,22 @@ public class MainMenuCtrl implements IViewController{
 
     //TODO: adding new credits fxml file to redirect to
     @FXML
-    void creditsPage(ActionEvent event) {
-        System.out.println("Software Development Methods Course Project");
+    void creditsPage(ActionEvent event) throws IOException {
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/credits.fxml"));
+        Parent root1 = fxmlLoader.load();
+        stage.setTitle("Credits Page");
+        stage.setScene(new Scene(root1));
+        stage.show();
     }
 
     @FXML
     void exitGame(ActionEvent event) {
+        Platform.exit();
+    }
+
+    @FXML
+    void closeCredits(ActionEvent event) {
         Platform.exit();
     }
 
