4f0ee56cc7616a7efaeff1b67a50bf86e1f8461c
azad <azad.sadr.h@gmail.com>
2021-05-23 10:31:38 +0200

    modified credits page which included in main_menu fxml file

--- src/main/java/takeiteasy/GUI/Controller/CreditsCtrl.java
+++
@@ -1,8 +0,0 @@
-package takeiteasy.GUI.Controller;
-
-import javafx.fxml.FXML;
-import javafx.stage.Stage;
-import javafx.stage.Window;
-
-public class CreditsCtrl {
-}
--- src/main/java/takeiteasy/GUI/Controller/MainMenuCtrl.java
+++ src/main/java/takeiteasy/GUI/Controller/MainMenuCtrl.java
@@ -1,31 +1,22 @@
 package takeiteasy.GUI.Controller;
 
 import javafx.application.Platform;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.stage.Stage;
+import javafx.scene.layout.AnchorPane;
 import org.json.JSONObject;
 import takeiteasy.GUI.IViewUpdater;
 import takeiteasy.game.IGame;
 
-import java.io.IOException;
-
-//TODO: how about implementing the initializable class
 public class MainMenuCtrl implements IViewController{
     IGame game;
     IViewUpdater vu;
 
     @FXML
-    Button start;
+    AnchorPane creditsPane;
 
     @Override
     public void injectGame(IGame g){
         this.game=g;
-        linkUI();
     }
 
     @Override
@@ -33,35 +24,24 @@ public class MainMenuCtrl implements IViewController{
         this.vu = vu;
     }
 
-    void linkUI(){
-        //TODO:link fxml objs actions to callbacks
-        //TODO: do we need this function?
-    }
-
     @FXML
-    void startLocalMatch(ActionEvent event) {
+    void onStartLocalMatchRelease() {
         game.createLocalLobby();
         vu.updateView();
     }
 
-    //TODO: adding new credits fxml file to redirect to
     @FXML
-    void creditsPage(ActionEvent event) throws IOException {
-        Stage stage = new Stage();
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/credits.fxml"));
-        Parent root1 = fxmlLoader.load();
-        stage.setTitle("Credits Page");
-        stage.setScene(new Scene(root1));
-        stage.show();
+    void onShowCreditsRelease() {
+        creditsPane.setVisible(true);
     }
 
     @FXML
-    void exitGame(ActionEvent event) {
-        Platform.exit();
+    void onCloseCreditsRelease(){
+        creditsPane.setVisible(false);
     }
 
     @FXML
-    void closeCredits(ActionEvent event) {
+    void onExitGameRelease() {
         Platform.exit();
     }
 
