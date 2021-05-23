6fd3ff9fcab1488f13da9bc373ce3604b1f8913c
MR <michelerisp@gmail.com>
2021-05-23 12:04:09 +0200

    Tile and Tilepool done

--- src/main/java/takeiteasy/tilepool/TilePool.java
+++ src/main/java/takeiteasy/tilepool/TilePool.java
@@ -3,12 +3,13 @@ package takeiteasy.tilepool;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Random;
+import java.util.stream.IntStream;
 
 public class TilePool implements ITilePool {
 
-    private final Tile[] extractedTiles;
-    public long seed;
-    private final static Integer size = 19;
+    final Tile[] extractedTiles;
+    long seed;
+    final static Integer size = 19;
 
     @Override
     public Integer getSize() {
@@ -29,19 +30,11 @@ public class TilePool implements ITilePool {
     }
 
     @Override
-    public void reset(long seed) {
+    public void reset(long newSeed) {
         Tile[] tileset = createFullTileSet();
-
-        //TODO:Collections.shuffle(Arrays.asList(tileset), new Random(this.seed));
-        this.seed = seed;
-        Random rand = new Random(seed);
-        Collections.shuffle(Arrays.asList(tileset), rand);
-
+        this.seed=newSeed;
+        Collections.shuffle(Arrays.asList(tileset),  new Random(this.seed));
         System.arraycopy(tileset, 0, extractedTiles, 0, size);
-    }
-
-    private static long generateSeed(){
-        return new Random().nextLong();
     }
 
     public TilePool(long seed){
@@ -51,20 +44,19 @@ public class TilePool implements ITilePool {
     }
 
     public TilePool() {
-        this(generateSeed());
+        this(new Random().nextLong());
     }
 
     private Tile[] createFullTileSet() {
         Tile[] tileSet = new Tile[27];
         try {
-            for (int i = 0; i < tileSet.length; ++i) {
-                tileSet[i] = new Tile(Tile.topValues[i / 9],
-                        Tile.leftValues[(i / 3) % 3],
-                        Tile.rightValues[i % 3]);
-            }
+            tileSet = IntStream.range(0,27).mapToObj(i->new Tile(Tile.topValues[i / 9],
+                    Tile.leftValues[(i / 3) % 3],
+                    Tile.rightValues[i % 3])).toArray(Tile[]::new);
         }
         catch (IllegalArgumentException ignored){
         }
+
         return tileSet;
     }
 }
