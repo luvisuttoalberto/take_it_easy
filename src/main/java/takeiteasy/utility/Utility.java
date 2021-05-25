1ea3b726b2c29b1e38b7c9c3b9cf5e71ea9b0a08
luvisuttoalberto <luvisutto.alberto@gmail.com>
2021-05-25 17:13:37 +0200

    Refactor: substituted for with stream in generateCoordinateStandard in Utility

--- src/main/java/takeiteasy/utility/Utility.java
+++ src/main/java/takeiteasy/utility/Utility.java
@@ -1,6 +1,9 @@
 package takeiteasy.utility;
 
 import takeiteasy.board.HexCoordinates;
+import takeiteasy.board.exceptions.BadHexCoordinatesException;
+
+import java.util.stream.IntStream;
 
 public class Utility {
     public static HexCoordinates[] generateCoordinateStandard(){
@@ -11,13 +14,17 @@ public class Utility {
                 {1, 1, -2}, {1, 0, -1}, {1, -1, 0}, {1, -2, 1},
                 {2, 0, -2}, {2, -1, -1}, {2, -2, 0}
         };
+
         HexCoordinates[] coords = new HexCoordinates[19];
-        try {
-            for (int j = 0; j < 19; ++j) {
-                coords[j] = new HexCoordinates(coordinateSet[j][0], coordinateSet[j][1], coordinateSet[j][2]);
-            }
-        } catch (Exception ignored) {
-        }
+        IntStream.range(0, coords.length)
+                .forEach(iii -> {
+                    try {
+                        coords[iii] = new HexCoordinates(  coordinateSet[iii][0],
+                                                            coordinateSet[iii][1],
+                                                            coordinateSet[iii][2]
+                                                    );
+                    } catch (BadHexCoordinatesException ignored) {}
+                });
         return coords;
     }
 }
